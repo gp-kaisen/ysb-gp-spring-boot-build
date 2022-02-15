c0f5f5c29e326ea76a5a6b5bc8ebdff110310530
daizhihong <daizhihong@yabang.cn>
2022-02-15 15:29:28 +0800

    [logger-starter]增加业务日志记录传输对象基类

---
+++ ysb-gp-spring-boot-logger-starter/src/main/java/com/ysb/gp/logger/business/model/BaseLogField.java
@@ -0,0 +1,35 @@
+package com.ysb.gp.logger.business.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.io.Serializable;
+
+/**
+ * @author laosy
+ * @date 2022/2/11
+ */
+@SuperBuilder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseLogField implements Serializable {
+    /**
+     * 业务id
+     */
+    private String bid;
+    /**
+     * 主要行为
+     */
+    private String action;
+    /**
+     * 用户id
+     */
+    private String userId;
+    /**
+     * 诊所id
+     */
+    private String clinicId;
+}
